e6f7af15afb1b32a5015b215084188e563e2f459
cmacana <carlos.macana@zepben.com>
2021-10-05 13:08:42 +1100

    Adding: config reading in main.py & Removing test folder
    Signed-off-by: cmacana <carlos.macana@zepben.com>

--- src/connect_to_server.py
+++ src/connect_to_server.py
@@ -7,13 +7,15 @@ rpc_port = 443
 def connecting_server(config):
     print("Connecting to Server...")
 
+    # Reading certificate
     with open(config['ewb_server']['ca_path'], "rb") as f:
         ca = f.read()
 
-    with connect(host=host, rpc_port=rpc_port,
-                 conf_address="https://evolve.essential.zepben.com/ewb/auth",
-                 client_id="8LCZNel8deS6Rcpt9Fv4ZgVMCzXI9uJ3",
-                 username="somerandomuser@bouckaert.com.au",
-                 password="Giraffe1354211", secure=True, ca=ca) as channel:
+    # Connecting server
+    with connect(host=config['ewb_server']['host'], rpc_port=config['ewb_server']['rpc_port'],
+                 conf_address=config['auth0']['conf_address'],
+                 client_id=config['auth0']['client_id'],
+                 username=config['auth0']['username'],
+                 password=config['auth0']['password'], secure=True, ca=ca) as channel:
         client = SyncNetworkConsumerClient(channel)
         print("Connection Established")
--- test/test_web_client.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Chrome()
-driver.get("https://evolve.essential.zepben.com")
-assert "EWB App" in driver.title
