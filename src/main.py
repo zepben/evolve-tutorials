f149464445ebb0e0a8b58c85ac9bd84ec1ceb7d6
Tatiana <tatiana.bencardino@zepben.com>
2021-10-05 17:30:32 +1100

    Added copyright to files in src directory
    Signed-off-by: Tatiana <tatiana.bencardino@zepben.com>

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+#  Copyright 2020 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from eas.upload_studies import upload_study
 from evolve_sdk.connect_to_server import connecting_server
 from evolve_sdk.creating_objects import creating_objects
