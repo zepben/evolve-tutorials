ad82492fc256351e7ca838099dcc65cbca07c849
cmacana <carlos.macana@zepben.com>
2021-10-05 12:07:30 +1100

    Adding: Reading local certificate
    Signed-off-by: cmacana <carlos.macana@zepben.com>

--- src/connect_to_server.py
+++ src/connect_to_server.py
@@ -3,26 +3,14 @@ from zepben.evolve import *
 host = "evolve.essential.zepben.com"
 rpc_port = 443
 
+with open("E:\evolve-tutorials\certificates\lets-encrypt-r3.pem", "rb") as f:
+    ca = f.read()
+
 print("Connecting to Server")
 with connect(host=host, rpc_port=rpc_port,
              conf_address="https://evolve.essential.zepben.com/ewb/auth",
              client_id="8LCZNel8deS6Rcpt9Fv4ZgVMCzXI9uJ3",
              username="somerandomuser@bouckaert.com.au",
-             password="Giraffe1354211", secure=True) as channel:
+             password="Giraffe1354211", secure=True, ca=ca) as channel:
     client = SyncNetworkConsumerClient(channel)
     print("Connection Established")
-
-    feeder_mrid = "CPM3B3"
-    result = client.get_equipment_container(mrid=feeder_mrid)
-    container: NetworkService = client.service
-
-power_transformers: [PowerTransformer] = list(container.objects(PowerTransformer))
-energy_consumers: [EnergyConsumer] = list(container.objects(EnergyConsumer))
-pv_units: [PhotoVoltaicUnit] = list(container.objects(PhotoVoltaicUnit))
-batteries: [BatteryUnit] = list(container.objects(BatteryUnit))
-
-print(f'Power Transformers in the feeder {feeder_mrid}: {len(power_transformers)}')
-print(f'Energy Consumers in the feeder {feeder_mrid}: {len(energy_consumers)}')
-print(f'Photovoltaic Units in the feeder {feeder_mrid}: {len(pv_units)}')
-print(f'Batteries in the feeder {feeder_mrid}: {len(batteries)}')
-
